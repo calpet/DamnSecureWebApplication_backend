e4e14cf3f370c97ef1a001cbcf64aee3acd98542
Calin Peters <calinpeters98@hotmail.nl>
2021-12-07 12:15:54 +0100

    Initial commit

---
+++ backend/src/main/java/com/dswa/backend/DswaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dswa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dswa/backend/DswaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dswa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
